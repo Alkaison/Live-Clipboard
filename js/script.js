e369f528669d207268059cf7c51b5b3bcd3742bd
Alkaison <505ganeshmourya@gmail.com>
2023-10-14 21:11:33 +0530

    fix: humburger from right side

--- js/script.js
+++ js/script.js
@@ -5,9 +5,15 @@ const crossIcon = document.querySelector(".cross-icon");
 
 // toggle navbar on mobile
 const toggleNavbarContainer = () => {
-    navContainer.classList.toggle("active");
-}
+  navContainer.classList.toggle("active");
+};
 
 // event listeners for navbar clicks
 humBurgerIcon.addEventListener("click", toggleNavbarContainer);
 crossIcon.addEventListener("click", toggleNavbarContainer);
+
+// Create Clipboard button
+const createBtn = document.querySelector("#createRoom");
+createBtn.addEventListener("click", () => {
+  window.location.href = "./clipboard.html";
+});
