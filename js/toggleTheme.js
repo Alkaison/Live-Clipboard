c113cbdb65ebaeaee5c7ed6169e1fa3911f37d1d
Alkaison <505ganeshmourya@gmail.com>
2023-10-15 20:29:43 +0530

    feat: added theme toggle to all pages

---
+++ js/toggleTheme.js
@@ -0,0 +1,24 @@
+//Toggle light and dark theme
+const theme = document.querySelector(".theme");
+theme.onclick = function () {
+  document.body.classList.toggle("light-theme");
+  if (document.body.classList.contains("light-theme")) {
+    theme.src = "assets/moon.png";
+    let darktheme = 0;
+    localStorage.setItem("isDark", darktheme);
+  } else {
+    theme.src = "assets/sun.png";
+    let darktheme = 1;
+    localStorage.setItem("isDark", darktheme);
+  }
+};
+
+//Get preferred theme from local storage
+let isDark = localStorage.getItem("isDark");
+if (isDark == 0) {
+  document.body.classList.add("light-theme");
+  theme.src = "assets/moon.png";
+} else {
+  document.body.classList.remove("light-theme");
+  theme.src = "assets/sun.png";
+}
