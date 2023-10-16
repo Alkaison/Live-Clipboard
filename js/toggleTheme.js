edb127cf4784b6cd2dab1824512e100ca5ba6fd3
Alkaison <505ganeshmourya@gmail.com>
2023-10-16 10:24:53 +0530

    feat: updated assets to webp format

--- js/toggleTheme.js
+++ js/toggleTheme.js
@@ -3,11 +3,11 @@ const theme = document.querySelector(".theme");
 theme.onclick = function () {
   document.body.classList.toggle("light-theme");
   if (document.body.classList.contains("light-theme")) {
-    theme.src = "assets/moon.png";
+    theme.src = "assets/moon.webp";
     let darktheme = 0;
     localStorage.setItem("isDark", darktheme);
   } else {
-    theme.src = "assets/sun.png";
+    theme.src = "assets/sun.webp";
     let darktheme = 1;
     localStorage.setItem("isDark", darktheme);
   }
@@ -17,8 +17,8 @@ theme.onclick = function () {
 let isDark = localStorage.getItem("isDark");
 if (isDark == 0) {
   document.body.classList.add("light-theme");
-  theme.src = "assets/moon.png";
+  theme.src = "assets/moon.webp";
 } else {
   document.body.classList.remove("light-theme");
-  theme.src = "assets/sun.png";
+  theme.src = "assets/sun.webp";
 }
