5b9b35853fb249c460600566cf5d4de59a3ead58
Alkaison <505ganeshmourya@gmail.com>
2023-12-08 20:53:26 +0530

    feat: separated validation code for clipboard and firebase config file

---
+++ js/validateRoomCode.js
@@ -0,0 +1,61 @@
+const createClipboardBtn = document.querySelector(".btn-1");
+const joinClipboardBtn = document.querySelector(".btn-2");
+const codeInputField = document.querySelector("#joinRoom");
+const codeInputLabel = document.querySelector("#lbl-2");
+const codeInputContainer = document.querySelector(".join-room-container");
+
+let joiningCode = "";
+const JOINING_CODE_LENGTH = 5;
+
+const generateJoiningCode = () => {
+  let createCode = "";
+  const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const strLength = str.length;
+
+  for (let i = 0; i < JOINING_CODE_LENGTH; i++) {
+    createCode += str.charAt(Math.floor(Math.random() * strLength));
+  }
+
+  invalidCodeError(false);
+  codeInputField.value = "";
+  localStorage.setItem("code", createCode);
+  window.location.href = "./clipboard.html";
+};
+
+// Update the joining code based on input
+const updateJoiningCode = (value) => {
+  joiningCode = value;
+};
+
+const invalidCodeError = (value) => {
+  if (value) {
+    codeInputContainer.classList.add("active");
+    codeInputLabel.textContent = "Invalid ID, please check again.";
+  } else {
+    codeInputContainer.classList.remove("active");
+    codeInputLabel.textContent = "Or join an existing one...";
+  }
+};
+
+// Validate the joining code
+const validateJoiningCode = () => {
+  const codeLength = joiningCode.length;
+  const regExp = /^[a-zA-Z0-9]+$/;
+  const validCode =
+    codeLength === JOINING_CODE_LENGTH && regExp.test(joiningCode);
+
+  if (validCode) {
+    invalidCodeError(false);
+    localStorage.setItem("code", joiningCode);
+    window.location.href = "./clipboard.html";
+  } else {
+    invalidCodeError(true);
+  }
+};
+
+// listeners for home page
+createClipboardBtn.addEventListener("click", generateJoiningCode);
+joinClipboardBtn.addEventListener("click", validateJoiningCode);
+codeInputField.addEventListener("input", (e) =>
+  updateJoiningCode(e.target.value)
+);
