b01021e5286f2a5ca9e3cda7e0c7e0324b92996a
Alkaison <505ganeshmourya@gmail.com>
2024-02-11 13:31:30 +0530

    fix: pwa app code bugs

--- public/sw.js
+++ public/sw.js
@@ -1,76 +1,71 @@
-// cache app name with version stages
-const cacheData = "realtime-clipboard-cache-v2";
+const cacheDataName = "realtime-clipboard-cache-v4";
 
-// Add the URLs of the Google Fonts you want to cache here
-const fontUrls = [
-  "https://fonts.googleapis.com/css2?family=Rubik&display=swap",
-  "https://fonts.googleapis.com/css2?family=Poppins&display=swap",
+const staticAssets = [
+  // "/static/js/main.272abdc0.js",
+  // "/static/css/main.72ee3a12.css",
+  "/static/js/bundle.js",
+  "/assets/clipboard-logo.webp",
+  "/assets/clipboard.webp",
+  "/assets/no-internet.webp",
+  "/assets/avatar-Alkaison.webp",
+  "/assets/avatar-Uzumaki.webp",
+  "/assets/moon.webp",
+  "/assets/sun.webp",
+  "/assets/copy.svg",
+  "/favicon/android-chrome-192x192.png",
+  "/favicon/android-chrome-512x512.png",
+  "/favicon/apple-touch-icon.png",
+  "/favicon/favicon-16x16.png",
+  "/favicon/favicon-32x32.png",
+  "/favicon/favicon.ico",
+  "/favicon/site.webmanifest",
+  "/manifest.json",
+  "/index.html",
+  "/",
 ];
 
-// store the files into cache storage
-this.addEventListener("install", (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheData).then((cache) => {
-      cache.addAll([
-        "/static/js/main.272abdc0.js",
-        "/static/css/main.72ee3a12.css",
-        "/assets/clipboard-logo.webp",
-        "/assets/clipboard.webp",
-        "/assets/no-internet.webp",
-        "/assets/avatar-Alkaison.webp",
-        "/assets/avatar-Uzumaki.webp",
-        "/assets/moon.webp",
-        "/assets/sun.webp",
-        "/assets/copy.svg",
-        "/favicon/android-chrome-192x192.png",
-        "/favicon/android-chrome-512x512.png",
-        "/favicon/apple-touch-icon.png",
-        "/favicon/favicon-16x16.png",
-        "/favicon/favicon-32x32.png",
-        "/favicon/favicon.ico",
-        "/favicon/site.webmanifest",
-        "/index.html",
-        "/",
-      ]);
-
-      // Cache Google Fonts
-      fontUrls.forEach((fontUrl) => {
-        fetch(new Request(fontUrl, { mode: "cors" })) // Use 'cors' mode for Google Fonts
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`Failed to fetch ${fontUrl}`);
-            }
-            cache.put(fontUrl, response);
-          })
-          .catch((error) => console.error(error));
-      });
+    caches.open(cacheDataName).then((cache) => {
+      return cache.addAll(staticAssets);
     })
   );
 
-  this.skipWaiting();
+  self.skipWaiting();
 });
 
-// remove old caches when a new service worker is activated
-this.addEventListener("activate", (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== cacheData).map((key) => caches.delete(key))
+        keys
+          .filter((key) => key !== cacheDataName)
+          .map((key) => caches.delete(key))
       );
     })
   );
 });
 
-// fetch files from cache storage
-this.addEventListener("fetch", (event) => {
-  if (!navigator.onLine) {
-    event.respondWith(
-      caches.match(event.request).then((res) => {
-        if (res) {
-          return res;
-        }
-        return fetch(event.request).catch(() => caches.match("/offline.html"));
-      })
-    );
-  }
+// Prioritize network for fetches with a fallback to cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    // Check if request is for an asset in the cache
+    caches.match(event.request).then((cachedResponse) => {
+      // Cache hit (return cached response)
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // Network request with caching
+      return fetch(event.request).then((fetchResponse) => {
+        const clonedResponse = fetchResponse.clone(); // Clone for caching before use
+
+        caches.open(cacheDataName).then((cache) => {
+          cache.put(event.request, clonedResponse); // Cache the cloned response
+        });
+
+        return fetchResponse; // Return the original response for immediate use
+      });
+    })
+  );
 });
--- src/swDev.js
+++ src/swDev.js
@@ -1,34 +1,18 @@
+/**
+ * The function `swDev` registers a service worker if it is supported in the browser, otherwise it logs
+ * an error message.
+ */
 export default function swDev() {
-  // Check for Service Worker support
   if ("serviceWorker" in navigator) {
-    const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
-
-    // Register the Service Worker
     navigator.serviceWorker
-      .register(swUrl, { scope: "/" })
+      .register("/sw.js")
       .then((registration) => {
-        registration.onupdatefound = () => {
-          const installingWorker = registration.installing;
-          installingWorker.onstatechange = () => {
-            if (installingWorker.state === "installed") {
-              if (navigator.serviceWorker.controller) {
-                // A new service worker is available and has taken control
-                // You may want to notify the user to refresh the page for the update
-                // console.log("New content is available; please refresh.");
-              } else {
-                // The service worker is installed but not yet taking control
-                // console.log("Content is cached for offline use.");
-              }
-            }
-          };
-        };
-
-        // console.log("Service Worker registered successfully:", registration);
+        console.log("Service worker registered successfully:", registration);
       })
       .catch((error) => {
-        console.error("Error during Service Worker registration:", error);
+        console.error("Service worker registration failed:", error);
       });
   } else {
-    console.error("Service workers are not supported in this browser.");
+    console.error("Service worker is not supported in this browser.");
   }
 }
