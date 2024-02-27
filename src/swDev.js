42c8253bdbf95efd6c2c0fcdd665f78d735386e6
Alkaison <505ganeshmourya@gmail.com>
2024-02-27 16:56:37 +0530

    updated assets for release icons into service worker for PWA

--- src/swDev.js
+++ src/swDev.js
@@ -7,7 +7,7 @@ export default function swDev() {
     navigator.serviceWorker
       .register("/sw.js")
       .then((registration) => {
-        console.log("Service worker registered successfully:", registration);
+        // console.log("Service worker registered successfully:", registration);
       })
       .catch((error) => {
         console.error("Service worker registration failed:", error);
