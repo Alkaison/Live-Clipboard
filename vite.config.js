b57e47474ea54ce967fb56b45fbbe79d5a20a58b
Alkaison <505ganeshmourya@gmail.com>
2024-08-04 20:28:44 +0530

    chore: remove console logs from production build files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import removeConsole from "vite-plugin-remove-console";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeConsole()],
   server: {
     port: 3000,
     open: true,
